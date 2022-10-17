b78e92a5001575a6224e0129da52e75222a6f736
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-10-17 08:03:01 -0700

    Fixed Bugs

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ person = {
   lastName: 'string'
 }
 newFirstName : "string"
-*/change firstName of person object to newFirstName
+change firstName of person object to newFirstName*/
 
 
 function setFirstName(person, newFirstName) {
@@ -78,56 +78,62 @@ setAge(person4, 15)
 console.log(person3);
 console.log(person4);
 
-//6. giveBirthday
-let persons1 = {
-  firstName: 'Nathalie',
-  lastName: 'Mitchell',
-  age: 31
+// GIVE BIRTHDAY
+console.log("\n Give Birthday");
+function giveBirthday(person){
+	if (person.age === undefined){
+		person.age = 1;
+	}else{
+		person.age++;
+	}
 }
-let persons2 = {
-  firstName: 'Bryden',
-  lastName: 'Mitchell',
-  age: 8
-}
-let persons3 = {
-  firstName: 'Mario',
-  lastName: 'Mitchell',
-  age: 31
-}
+let homie1 = {firstName: 'Bryden', lastName: 'Mitchell', age: 8};
+let homie2 = {firstName: 'Nathalie', lastName: 'Mitchell', age: 31};
+let homie3 = {firstName: 'Mario', lastName: 'Mitchell'}
+giveBirthday(homie1);
+giveBirthday(homie2);
+giveBirthday(homie3);
+console.log(homie1);
+console.log(homie2);
+console.log(homie3);
 
-function giveBirthday(persons) {
-  if (persons.age === undefined) {
-    persons.age = 1;
-  } else persons.age += 1
-  return persons
+
+// MARRY
+console.log("\n Marry");
+function marry(marry1, marry2){
+	if (marry1.married === false && marry2.married === false){
+		marry1.married = true;
+		marry2.married = true;
+	}
+	if (marry1.spouseName === undefined){
+		marry1.spouseName = `${marry2.firstName} ${marry2.lastName}`
+	}
+	if (marry2.spouseName === undefined){
+		marry2.spouseName = `${marry1.firstName} ${marry1.lastName}`
+	}
 }
-giveBirthday(persons1)
-giveBirthday(persons2)
-giveBirthday(persons3)
+let marry1 = {firstName: 'Jake', lastName: 'The Human', married: false}
+let marry2 = {firstName: 'Princess', lastName: 'BubbleGum', married: false}
+marry(marry1, marry2)
+console.log(marry1) 
+console.log(marry2) 
 
-//7. marry
-const persons4 = { firstName: 'Jake', lastName: 'The Human', married: false }
-const persons5 = { firstName: 'Princess', lastName: 'BubbleGum', married: false }
-function marry(persons4, persons5) {
-  persons4.married = true;
-  persons5.married = true;
 
-  persons4.spouseName = `${persons5.firstName} ${persons5.lastName}`
-  persons5.spouseName = `${persons4.firstName} ${persons4.lastName}`
+// DIVORCE
+console.log("\n Divorce");
+function divorce(div1, div2){
+	if (div1.married === true && div2.married === true){
+		div1.married = false;
+		div2.married = false;
+	}
+	delete div1.spouseName;
+	delete div2.spouseName;
 }
-console.log(marry(persons4, persons5))
-
-//8. divorce
-const person6 = { firstName: 'Jake', lastName: 'The Human', married: false }
-const person7 = { firstName: 'Princess', lastName: 'BubbleGum', married: false }
-function divorce(person6, person7) {
-  person6.married = false;
-  person7.married = false;
-
-  delete person6.spouseName
-  delete person7.spouseName
-}
-divorce(person6, person7)
+const div1 = {firstName: 'Jake', lastName: 'The Human', married: true, spouseName: 'Petra Solano'}
+const div2 = {firstName: 'Princess', lastName: 'BubbleGum', married: true, spouseName: 'Colin Jaffe'}
+divorce(div1, div2);
+console.log(div1)  
+console.log(div2) 
 
 
 
